6e8259961a44cbe9ee76bba3c2089f8d7f105651
test <test@example.com>
2020-09-12 05:58:21 +1000

    Completed the todolist project

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 window.onload = function(){
  const addInput = document.getElementById('add-input');
  const addButton = document.getElementById('add-button');
-
+ const ul = document.querySelector('#list');
  const addItemHandler = () => {
      let inputValue = addInput.value;
-     console.log(inputValue);
+     if(!inputValue) {
+         return;
+     }
+
+ const finishTaskHandler = (e) => {
+     if(e.target.checked){
+        e.target.setAttribute('class', 'strike');
+     } else {
+        e.target.removeAttribute('class');
+     }
+ }
+
+     const template = document.getElementById('template');
+     const clone = document.importNode(template.content, true);
+     clone.querySelector('span').textContent = inputValue;
+     clone.querySelector('input').addEventListener('click', finishTaskHandler);
+     ul.appendChild(clone);
+     addInput.value = '';
+
  }
  addButton.addEventListener('click', addItemHandler);
 };
